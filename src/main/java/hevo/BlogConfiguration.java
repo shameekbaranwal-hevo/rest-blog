0b914668294ac11a81f34a021926b3ed2549c428
Shameek Kumar Baranwal <shameek.baranwal@hevodata.com>
2023-07-27 12:06:38 +0530

    Added JDBI.

--- src/main/java/hevo/BlogConfiguration.java
+++ src/main/java/hevo/BlogConfiguration.java
@@ -5,28 +5,25 @@ import io.dropwizard.core.Configuration;
 import jakarta.validation.constraints.NotEmpty;
 
 public class BlogConfiguration extends Configuration {
-    @NotEmpty
-    private String template;
+  @NotEmpty private String template;
 
-    @NotEmpty
-    private String defaultName;
-    
+  @NotEmpty private String defaultName;
 
-    @JsonProperty
-    public String getTemplate() {
-        return template;
-    }
+  @JsonProperty
+  public String getTemplate() {
+    return template;
+  }
 
-    @JsonProperty
-    public String getDefaultName() {
-        return defaultName;
-    }
+  @JsonProperty
+  public String getDefaultName() {
+    return defaultName;
+  }
 
-    public void setTemplate(String template) {
-        this.template = template;
-    }
+  public void setTemplate(String template) {
+    this.template = template;
+  }
 
-    public void setDefaultName(String defaultName) {
-        this.defaultName = defaultName;
-    }
+  public void setDefaultName(String defaultName) {
+    this.defaultName = defaultName;
+  }
 }
